71f669d1a8ec5765212ee4e1e31ede68b48712bd
RAPHAEL MARQUES RIBEIRO DOS SANTOS <140455000+RaphaMsk8@users.noreply.github.com>
2026-05-12 21:12:08 -0300

    ,

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
         "particles": {
             "number": { "value": 100 },
             "color": { "value": "#38bdf8" },
+            "size": { "value": 2, "random": true },
             "line_linked": { "enable": true, "distance": 150, "color": "#38bdf8", "opacity": 0.2 },
             "move": { "enable": true, "speed": 1.5 }
         },
